00b78d2223f71508eadb04464392d725d4dad39b
Faysal Hossain <munnafh11@gmail.com>
2023-09-03 01:53:50 +0600

    Add linear search

--- src/javas/proglem/loop_Programs/Reverse_String_P_12.java
+++ src/javas/proglem/loop_Programs/Reverse_String_P_12.java
@@ -3,11 +3,11 @@ package javas.proglem.loop_Programs;
 public class Reverse_String_P_12 {
     public static void main(String[] args) {
 
-        String name ="munna";
+        String name ="I am a Developer";
         String rev="";
 
         for (int i=name.length()-1;i>=0;i--){
-            rev= rev +name.charAt(i);
+            rev= rev + name.charAt(i);
         }
         System.out.println(rev);
     }
